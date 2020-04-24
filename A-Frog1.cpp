8c9d760453f07b926ac0980b354404ccd322c3b8
Pradeep Singh <pswaldia1@gmail.com>
2020-04-24 15:42:08 +0530

    Create A-Frog1.cpp

---
+++ A-Frog1.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define str(a) to_string(a)
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define SORT(c) sort(c.begin(),c.end())
+#define max_heap priority_queue<int>
+#define min_heap priority_queue< int,vector<int>, greater<int> >
+#define mod 1000000007
+#define setbits(x) __builtin_popcount(x)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define start(t) int x;cin>>x;while(x--)
+#define inf LLONG_MAX
+#define sync ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl "\n"
+
+
+
+
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n; cin >> n;
+	vector<int> h(n + 1);
+	for (int i = 1; i <= n; i++)
+		cin >> h[i];
+	h[0] = 0;
+	int dp[n + 1];
+	dp[1] = 0;
+	dp[2] = abs(h[2] - h[1]);
+	for (int i = 3; i <= n; i++) {
+		dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]));
+	}
+	cout << dp[n] << endl;
+
+}
+
+
+
+
+
+
+
+
+
